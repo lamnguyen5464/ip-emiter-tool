a8d08aa36652902e2708ed55ed85c83ffcbd1500
lamnguyen5464 <lamnguyen5464@gmail.com>
2022-02-21 15:46:14 +0700

    feat: emited successfully

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 const { exec } = require("child_process");
-const axios = require("axios");
+const https = require("https");
 
-exec(
-  "ipconfig getifaddr en0 || ipconfig getifaddr en1",
-  (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
+const DELTA_TIME = 1000 * 60 * 15;
 
-    axios({
-      url: "https://chat.googleapis.com/v1/spaces/AAAAGWW6gyE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=hq-V-1ycDnsLv26y9RqPLnktaKXvME3pr5kN9yNPMz8%3D",
-      method: "post",
+setInterval(async () => {
+  try {
+    const ip = await getIp();
+    await emitToHangout(ip);
+    console.log("Emitted ", ip);
+  } catch (e) {
+    console.log(e);
+  }
+}, DELTA_TIME);
+
+const getIp = () => {
+  return new Promise((resolve, reject) => {
+    exec(
+      "ipconfig getifaddr en0 || ipconfig getifaddr en1",
+      (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.log(`error: ${error?.message || stderr}`);
+          reject(error?.message || stderr);
+          return;
+        }
+        resolve(stdout);
+      }
+    );
+  });
+};
+
+const emitToHangout = (text) =>
+  new Promise((resolve, reject) => {
+    const data = JSON.stringify({ text });
+    const options = {
+      hostname: "chat.googleapis.com",
+      path: `/v1/spaces/AAAAGWW6gyE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=hq-V-1ycDnsLv26y9RqPLnktaKXvME3pr5kN9yNPMz8%3D`,
+      port: 443,
+      method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      data: { stdout },
-    })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((e) => {
-        console.log(e);
-      }),
-      console.log(`stdout: ${stdout}`);
-  }
-);
+    };
+
+    const req = https.request(options, (res) => {
+      res.on("data", (d) => {
+        // process.stdout.write(d);
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+      return;
+    });
+
+    req.write(data);
+    req.end();
+    resolve();
+  });
